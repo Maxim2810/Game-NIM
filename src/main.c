127cbcc03551ee3d2bfb344a075722f12dc3ef61
dimdimich123 <Frostm333@gmail.com>
2019-05-01 14:38:22 +0700

    Written main menu for the game in <main.c>

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include "../library/lib.h"
+
+int main()
+{
+    char i;
+    srand(time(NULL));
+    while (1) {
+        initscr();
+        printw("1-play\n2-records\n3-manual\n4-exit\n");
+        refresh();
+        i = getch();
+        switch (i) {
+        case 49:
+            break;
+        case 50:
+            break;
+        case 51:
+            clear();
+            printw("Nim - eto igra\nUpravlenie:\nn - "
+                   "stroka\nm - kolvo kamushek\n");
+            refresh();
+            getch();
+            clear();
+            break;
+        case 52:
+            clear();
+            endwin();
+            return 0;
+        }
+        clear();
+    }
+    return 0;
+}
