0dfa502c02d378dcd73b1726e9e49cce10c74990
dimdimich123 <Frostm333@gmail.com>
2019-05-01 15:22:07 +0700

    Added function <game ()> that starts the game

--- src/game.c
+++ src/game.c
@@ -2,6 +2,16 @@
 #include "../library/lib.h"
 #include "menu.h"
 
+void games(int** A, int* B, int jk)
+{
+    clear();
+    int win = 2;
+    while (1) {
+        break;
+    }
+    return;
+}
+
 void standart(int jk)
 {
     int i, j, **A, *B;
@@ -16,6 +26,7 @@ void standart(int jk)
     B[1] = 3;
     B[2] = 5;
     B[3] = 7;
+    games(A, B, jk);
     for (i = 0; i < jk; i++)
         free(A[i]);
     free(A);
--- src/game.h
+++ src/game.h
@@ -1,4 +1,5 @@
 #ifndef _SRC_GAME_H
 #define _SRC_GAME_H
+void games(int** A, int* B, int jk);
 void standart(int jk);
 #endif
