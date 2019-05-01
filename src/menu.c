c3cfaea1acb8725d2f3434402e037e8c1b45b85b
dimdimich123 <Frostm333@gmail.com>
2019-05-01 15:34:16 +0700

    Added function <pole>, which displays the playing field

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,35 @@
 #include "../library/lib.h"
 #include "game.h"
 
+void pole(int** A, int* B, int flag, int jk)
+{
+    int j;
+    clear();
+    printw("1: ");
+    for (j = 0; j < B[0]; j++)
+        printw(" %d|", A[0][j]);
+    printw("\n2: ");
+    for (j = 0; j < B[1]; j++)
+        printw(" %d|", A[1][j]);
+    printw("\n3: ");
+    for (j = 0; j < B[2]; j++)
+        printw(" %d|", A[2][j]);
+    printw("\n");
+    if (jk == 4) {
+        printw("4: ");
+        for (j = 0; j < B[3]; j++)
+            printw(" %d|", A[3][j]);
+        printw("\n");
+    }
+    if (flag)
+        printw("Now your turn\n");
+    else {
+        printw("Now computer turn\n");
+        getch();
+    }
+    return;
+}
+
 void difficulty()
 {
     int dif;
--- src/menu.h
+++ src/menu.h
@@ -1,4 +1,5 @@
 #ifndef _SRC_MENU_H
 #define _SRC_MENU_H
+void pole(int** A, int* B, int flag, int jk);
 void difficulty();
 #endif
