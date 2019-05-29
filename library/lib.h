15e14a507efe1ed6454c66d388a432533236fd2c
Maxim2810 <ghostguy@bk.ru>
2019-05-29 20:57:00 +0700

    Change library for graphics sfml

--- library/lib.h
+++ library/lib.h
@@ -1,6 +1,6 @@
 struct S{
 	int lim;
-	char names[20];
+	char names[250];
 };
 #include "curses.h"
 #include <math.h>
@@ -8,3 +8,6 @@ struct S{
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+#include <sstream>
