20f6d5e096c9f4e017a7fe3e70ff880bcb9bb9c2
Maxim2810 <ghostguy@bk.ru>
2019-05-30 15:25:53 +0700

    Add test lib

---
+++ test/functest.h
@@ -0,0 +1,7 @@
+#ifndef _TEST_FUNCTEST_H
+#define _TEST_FUNCTEST_H
+void BubbleSort(int* A, int n);
+void ComputerTurn(int* B, int jk, int s);
+int winner(int* B, int w, int jk);
+void games(int* B, int jk);
+#endif
